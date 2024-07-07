755db6ed81486363263279b9bb0732c20b7837f8
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2024-07-07 16:00:00 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use gst::prelude::*;
+use gstreamer as gst;
+
 fn main() {
-    println!("Hello, world!");
+    gst::init().unwrap();
+
+    let source = gst::ElementFactory::make("videotestsrc").build().unwrap();
+    // let enc = gst::ElementFactory::make("x264enc").build().unwrap();
+    let sink = gst::ElementFactory::make("autovideosink").build().unwrap();
+
+    let pipeline = gst::Pipeline::with_name("test-pipeline");
+    pipeline.add_many(&[&source, &sink]).unwrap();
+
+    gst::Element::link_many(&[&source, &sink]).unwrap();
+
+    pipeline.set_state(gst::State::Playing).unwrap();
+
+    let bus = pipeline.bus().unwrap();
+    for msg in bus.iter_timed(gst::ClockTime::NONE) {
+        use gst::MessageView;
+        match msg.view() {
+            MessageView::Eos(_) => break,
+            MessageView::Error(err) => {
+                eprintln!(
+                    "Error from {:?}: {} ({:?})",
+                    msg.src().map(|s| s.path_string()),
+                    err.error(),
+                    err.debug()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
 }
