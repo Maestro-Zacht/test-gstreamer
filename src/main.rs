02bed041a9b8884b7846c41055e65da977b14149
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2024-07-14 11:40:14 +0200

    websocket init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::thread;
+
 use gst::prelude::*;
 use gst::MessageView;
 use gstreamer as gst;
+use websocket::sync::Server;
 
 use clap::{Args, Parser, Subcommand};
+use websocket::ClientBuilder;
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -12,13 +16,13 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    Send(SendArgs),
-    Recv,
+    Send,
+    Recv(ServerArgs),
 }
 
 #[derive(Args, Debug)]
-struct SendArgs {
-    ips: Vec<String>,
+struct ServerArgs {
+    ip: String,
 }
 
 fn main() {
@@ -26,15 +30,17 @@ fn main() {
     gst::init().unwrap();
 
     let pipeline = match cli.command {
-        Commands::Send(SendArgs { ips }) => {
+        Commands::Send => {
             let source = gst::ElementFactory::make("ximagesrc")
-            .property("use-damage", false)
-            .build()
-            .unwrap();
+                .property("use-damage", false)
+                .build()
+                .unwrap();
             let capsfilter = gst::ElementFactory::make("capsfilter")
-                .property("caps", gst::Caps::builder("video/x-raw")
-                    .field("framerate", &gst::Fraction::new(30, 1))
-                    .build()
+                .property(
+                    "caps",
+                    gst::Caps::builder("video/x-raw")
+                        .field("framerate", &gst::Fraction::new(30, 1))
+                        .build(),
                 )
                 .build()
                 .unwrap();
@@ -46,12 +52,9 @@ fn main() {
             let pay = gst::ElementFactory::make("rtph264pay").build().unwrap();
             let sink = gst::ElementFactory::make("multiudpsink").build().unwrap();
 
-            for ip in ips {
-                sink.emit_by_name_with_values("add", &[ip.into(), 9001.into()]);
-            }
-
-            // sink.emit_by_name_with_values("add", &["192.168.171.69".into(), 9001.into()]);
-            // sink.emit_by_name_with_values("add", &["192.168.161.163".into(), 9001.into()]);
+            // for ip in ips {
+            //     sink.emit_by_name_with_values("add", &[ip.into(), 9001.into()]);
+            // }
 
             let tee = gst::ElementFactory::make("tee").build().unwrap();
 
@@ -62,14 +65,52 @@ fn main() {
             let videosink = gst::ElementFactory::make("autovideosink").build().unwrap();
 
             let pipeline = gst::Pipeline::with_name("send-pipeline");
-            pipeline.add_many(&[&source, &capsfilter, &tee, &queue1, &queue2, &videoconvert, &enc, &pay, &sink, &videoconvert2, &videosink]).unwrap();
+            pipeline
+                .add_many(&[
+                    &source,
+                    &capsfilter,
+                    &tee,
+                    &queue1,
+                    &queue2,
+                    &videoconvert,
+                    &enc,
+                    &pay,
+                    &sink,
+                    &videoconvert2,
+                    &videosink,
+                ])
+                .unwrap();
 
-            gst::Element::link_many(&[&source, &capsfilter, &tee, &queue1, &videoconvert, &enc, &pay, &sink]).unwrap();
+            gst::Element::link_many(&[
+                &source,
+                &capsfilter,
+                &tee,
+                &queue1,
+                &videoconvert,
+                &enc,
+                &pay,
+                &sink,
+            ])
+            .unwrap();
             gst::Element::link_many(&[&tee, &queue2, &videoconvert2, &videosink]).unwrap();
+
+            thread::spawn(move || {
+                let server = Server::bind("0.0.0.0:9000").unwrap();
+                for connection in server.filter_map(Result::ok) {
+                    let client = connection.accept().unwrap();
+                    let ip = client.peer_addr().unwrap().ip().to_string();
+                    println!("New client: {}", ip);
+                }
+            });
 
             pipeline
         }
-        Commands::Recv => {
+        Commands::Recv(ServerArgs { ip }) => {
+            let client = ClientBuilder::new(&format!("ws://{}:9000", ip))
+                .unwrap()
+                .connect_insecure()
+                .unwrap();
+
             gst::parse::launch(
                 "udpsrc port=9001 caps=\"application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96\" ! rtph264depay ! decodebin ! videoconvert ! autovideosink"
             )
